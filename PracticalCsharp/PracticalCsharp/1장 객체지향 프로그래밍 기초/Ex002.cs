8e638fb8a9921bb6d317f0c3f1bffaad2393b1cf
KYJKY <kyj_3052@naver.com>
2022-06-03 02:03:53 +0900

    1.2 구조체

--- PracticalCsharp/PracticalCsharp/1장 객체지향 프로그래밍 기초/Ex002.cs
+++ PracticalCsharp/PracticalCsharp/1장 객체지향 프로그래밍 기초/Ex002.cs
@@ -11,6 +11,22 @@ namespace PracticalCsharp._1장_객체지향_프로그래밍_기초
     {
         static void Main(string[] args)
         {
+            // 구조체 예
+            DateTime date = new DateTime(2022, 6, 3);
+            int year = date.Year;
+
+            // 365일 후
+            DateTime daysAfter = date.AddDays(365);
+            Console.WriteLine(daysAfter.ToString());
+            
+            /*
+            
+            구조체와 클래스 차이 > 객체를 메모리 상에 저장하는 방식이 다르다
+            클래스: 변수가 있는 곳과 다른 곳에 객체의 영역이 확보되고 변수에는 그 참조가 저장됨 > 힙 영역에 할당된다.
+            구조체: 변수 자체에 객체가 저장된다. > 스택 영역에 할당된다.
+            + 구조체는 상속할 수 없다.
+
+             */
 
         }
     }
